4e34a9450539970bd5eea069269e89e1ea77f819
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-12-21 12:31:48 -0300

    [M02S07] Ex 01 - Mini-Projeto Veiculos - Criação projeto

---
+++ src/main/java/com/clamed/m2s07/M2s07Application.java
@@ -0,0 +1,13 @@
+package com.clamed.m2s07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s07Application.class, args);
+	}
+
+}
