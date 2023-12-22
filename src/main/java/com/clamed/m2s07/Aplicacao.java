8449f292426fd0a8aaaefd758ab358fe303ea24b
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-12-22 07:10:06 -0300

    [M02S07] Ex 06 - Mini-Projeto Veiculos - Gravação Multas

--- src/main/java/com/clamed/m2s07/Aplicacao.java
+++ src/main/java/com/clamed/m2s07/Aplicacao.java
@@ -1,6 +1,8 @@
 package com.clamed.m2s07;
 import com.clamed.m2s07.enums.TipoVeiculo;
+import com.clamed.m2s07.model.Multa;
 import com.clamed.m2s07.model.Veiculo;
+import com.clamed.m2s07.repository.MultaRepository;
 import com.clamed.m2s07.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,11 +13,21 @@ public class Aplicacao {
     @Autowired
     private VeiculoRepository veiculoRepo;
 
+    @Autowired
+    private MultaRepository multaRepo;
+
     public void executar() {
         Veiculo veiculo1 = new Veiculo("ABC-1234", TipoVeiculo.AUTOMOVEL, "Bat-Movel", 2022, "preto");
         Veiculo veiculo2 = new Veiculo("BCA-4321", TipoVeiculo.ONIBUS, "Enterprise", 1960, "prata");
         veiculoRepo.save(veiculo1);
         veiculoRepo.save(veiculo2);
+
+        Multa multa1 = new Multa("Farol apagado", "Gothan City", 250F, veiculo1);
+        Multa multa2 = new Multa("Insulfilm", "Gothan City", 100F, veiculo1);
+        Multa multa3 = new Multa("Excesso velocidade", "Hiper-espaço", 400F, veiculo2);
+        multaRepo.save(multa1);
+        multaRepo.save(multa2);
+        multaRepo.save(multa3);
     }
 }
 
